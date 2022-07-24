75f3bf0e966f265ca2212cea8d643e46d6390c45
Chethuuu <chethanaprasadi2000@gmail.com>
2022-07-24 14:33:56 +0530

    Add Styles

--- chart/src/App.js
+++ chart/src/App.js
@@ -11,24 +11,94 @@ import {
 
 const data = [
   {
-    task1: 4000,
-    task2: 1000,
+    task1: 200,
+    task3: 200,
+    task2: 200,
+    task4: 200,
   },
   {
-    task1: 1000,
-    task2: 4000,
+    task1: 490,
+    task3: 490,
+    task2: 510,
+    task4: 510,
   },
   {
-    task1: 4000,
-    task2: 2000,
+    task1: 510,
+    task3: 510,
+    task2: 490,
+    task4: 490,
   },
   {
-    task1: 800,
-    task2: 1000,
+    task1: 480,
+    task3: 480,
+    task2: 160,
+    task4: 160,
   },
   {
-    task1: 1500,
-    task2: 3000,
+    task1: 470,
+    task3: 470,
+    task2: 180,
+    task4: 180,
+  },
+  {
+    task1: 120,
+    task3: 120,
+    task2: 510,
+    task4: 510,
+  },
+  {
+    task1: 180,
+    task3: 180,
+    task2: 420,
+    task4: 420,
+  },
+  {
+    task1: 590,
+    task3: 590,
+    task2: 440,
+    task4: 440,
+  },
+  {
+    task1: 430,
+    task3: 430,
+    task2: 590,
+    task4: 590,
+  },
+  {
+    task1: 80,
+    task3: 80,
+    task2: 520,
+    task4: 520,
+  },
+  {
+    task1: 0,
+    task3: 0,
+    task2: 80,
+    task4: 80,
+  },
+  {
+    task1: 450,
+    task3: 450,
+    task2: 60,
+    task4: 60,
+  },
+  {
+    task1: 600,
+    task3: 600,
+    task2: 600,
+    task4: 600,
+  },
+  {
+    task1: 500,
+    task3: 500,
+    task2: 500,
+    task4: 500,
+  },
+  {
+    task1: 180,
+    task3: 180,
+    task2: 170,
+    task4: 170,
   },
 ];
 
@@ -36,7 +106,26 @@ function App() {
   return (
     <>
       <h3> &nbsp; Your Work Summary</h3>
-      <h4> &nbsp; Nov - July</h4>
+
+      <h4>
+        {" "}
+        <span class="left">&nbsp; Nov - July</span>
+      </h4>
+      <span class="right">
+        <button className="btn1" type="submit">
+          Daily
+        </button>
+        <button className="btn2">Monthly</button>&nbsp;&nbsp;&nbsp;
+      </span>
+
+      <h3>
+        <span class="right">
+          <span class="dot1"></span>&nbsp;This Month &nbsp;&nbsp;&nbsp;
+        </span>{" "}
+        <span class="right">
+          <span class="dot"></span>&nbsp;Last Month &nbsp;
+        </span>{" "}
+      </h3>
       <ResponsiveContainer width="98%" aspect={3}>
         <LineChart
           width={500}
@@ -49,7 +138,12 @@ function App() {
             bottom: 5,
           }}
         >
-          <CartesianGrid horizontal="true" vertical="" stroke="#243240" />
+          <CartesianGrid
+            strokeDasharray="10"
+            horizontal="true"
+            vertical=""
+            stroke="#243240"
+          />
 
           <YAxis tick={{ fill: "#A9A9A9	" }} />
           <Tooltip
@@ -58,27 +152,43 @@ function App() {
             cursor={false}
           />
           <Line
-            type="monotone"
+            type="line"
             dataKey="task1"
-            stroke="#8884d8"
+            stroke="#246CF9"
             strokeWidth="5"
-            dot={{ fill: "#2e4355", stroke: "#8884d8", strokeWidth: 2, r: 5 }}
+            dot={{ fill: "#2e4355", stroke: "#246CF9", strokeWidth: 2, r: 5 }}
             activeDot={{
               fill: "#2e4355",
-              stroke: "#8884d8",
-              strokeWidth: 5,
+              stroke: "#246CF9",
+              strokeWidth: 6,
               r: 10,
             }}
           />
+
           <Line
-            type="monotone"
+            type="line"
+            dataKey="task3"
+            filter="blur(14px)"
+            stroke="#246CF9"
+            strokeWidth="6"
+          />
+
+          <Line
+            type="line"
             dataKey="task2"
-            stroke="#8884d8"
-            strokeWidth="5"
-            dot={{ fill: "#2e4355", stroke: "#8884d8", strokeWidth: 2, r: 5 }}
+            stroke="#FA2256"
+            strokeWidth="6"
+          />
+          <Line
+            type="line"
+            dataKey="task4"
+            filter="blur(14px)"
+            stroke="#FA2256"
+            strokeWidth="6"
+            dot={{ fill: "#2e4355", stroke: "#FA2256", strokeWidth: 2, r: 5 }}
             activeDot={{
               fill: "#2e4355",
-              stroke: "#8884d8",
+              stroke: "#FA2256",
               strokeWidth: 5,
               r: 10,
             }}
@@ -87,11 +197,15 @@ function App() {
       </ResponsiveContainer>
       <h1> &nbsp; 9845</h1>
 
-      <div className="ch"> blur
+      <div class="card">
+        <div class="container">
+          {" "}
+          <br />
+          <span class="dot"></span>&nbsp;Last Month
+           <br /> <br /> 
+          <span class="dot1"></span>&nbsp;This Month
         </div>
-
-      
-
+      </div>
     </>
   );
 }
